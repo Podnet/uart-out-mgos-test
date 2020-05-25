7f9febddc612ff734256172c3b1f930c67fbd6b6
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-25 16:56:38 +0530

    UART setup and periodic writing to UART

--- src/main.c
+++ src/main.c
@@ -1,11 +1,35 @@
 #include "mgos.h"
 
-static void timer_cb(void *arg) {
-  // Repeating code goes here.
-  (void) arg;
+int uart_no = 0;
+
+static void timer_cb(void *arg)
+{
+    // Repeating code goes here.
+    if(mgos_uart_write_avail(uart_no)) {
+        mgos_uart_write(uart_no, "Hello World", 11);
+    }
+    (void)arg;
 }
 
-enum mgos_app_init_result mgos_app_init(void) {
-  mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
-  return MGOS_APP_INIT_SUCCESS;
+enum mgos_app_init_result mgos_app_init(void)
+{
+    LOG(LL_INFO, ("Preparing UART interface"));
+
+    struct mgos_uart_config ucfg;
+    mgos_uart_config_set_defaults(uart_no, &ucfg);
+
+    ucfg.baud_rate = 9600;
+    ucfg.rx_buf_size = 1500;
+    ucfg.tx_buf_size = 1500;
+
+    if (!mgos_uart_configure(uart_no, &ucfg))
+    {
+        LOG(LL_ERROR, ("Failed to configure UART%d", uart_no));
+    }
+
+    else {
+        mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+        LOG(LL_INFO, ("Handler set for pushing data periodically via UART."));
+    }
+    return MGOS_APP_INIT_SUCCESS;
 }
